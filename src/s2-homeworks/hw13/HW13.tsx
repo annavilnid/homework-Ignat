8ca20be1cb08240e3371686c7fae0c3769cbb690
“Anna <anna.vilnid@gmail.com>
2022-12-01 11:24:15 +0100

    hw13 setText test

--- src/s2-homeworks/hw13/HW13.tsx
+++ src/s2-homeworks/hw13/HW13.tsx
@@ -36,7 +36,7 @@ const HW13 = () => {
             .then((res) => {
                 setCode('Код 200!')
                 // setText('...всё ок) код 200 - обычно означает что скорее всего всё ок)')
-                setText('...всё)')
+                setText('...всё ок)')
                 setImage(success200)
                 // дописать
                 console.log('успешно')
@@ -44,15 +44,18 @@ const HW13 = () => {
             .catch((e) => {
                     if (e.response.status === 400) {
                         setCode('Код 400!')
-                        setText('Ты не отправил success в body вообще! ошибка 400 - обычно означает что скорее всего фронт отправил что-то не то на бэк!')
+                        //setText('Ты не отправил success в body вообще! ошибка 400 - обычно означает что скорее всего фронт отправил что-то не то на бэк!')
+                        setText('Ты не отправил success в body вообще!')
                         setImage(error400)
                     }else if (e.response.status === 500) {
                         setCode('Код 500!')
-                        setText('эмитация ошибки на сервере ошибка 500 - обычно означает что что-то сломалось на сервере, например база данных)')
+                        //setText('эмитация ошибки на сервере ошибка 500 - обычно означает что что-то сломалось на сервере, например база данных)')
+                        setText('эмитация ошибки на сервере')
                         setImage(error500)
                     } else {
                         setCode('Error!')
-                        setText('Network Error AxiosError')
+                        // setText('Network Error AxiosError')
+                        setText('Error')
                         setImage(errorUnknown)
                     }
                 // дописать
